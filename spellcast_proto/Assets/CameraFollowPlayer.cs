ab46db653861c04c9e2212cb58ba6d59c59f9f00
Yrtna <dummyauction42@gmail.com>
2020-09-08 21:36:49 +0200

    Meteor Ability Added / PlayerLookAtMouse controller

---
+++ spellcast_proto/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public int posYOffset = 15;
+
+    public int posZOffset = -10;
+
+    public int rotXOffset = 55;
+    
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        var position = playerTransform.position;
+        transform.position = new Vector3(position.x, position.y + posYOffset, position.z + posZOffset);
+    }
+}
---
+++ spellcast_proto/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var position = this.transform.position;
+        position = new Vector3(position.x + horizontal, position.y, position.z + vertical);
+        transform.position = position;
+        // this.transform.Translate(horizontal, 0, vertical);
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+ 
+        if(Physics.Raycast(ray,out hit,100))
+        {
+            transform.LookAt(new Vector3(hit.point.x,transform.position.y,hit.point.z));
+        }
+    }
+}
